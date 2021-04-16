37d064436ea20059c459dc30fa833e35c6ee92bc
prasanth-kumar1 <prasanth.kumar@iiitb.org>
2021-04-16 14:25:12 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hellooo!!</h1>
+      {/* Header */}
+      <Header />
+      {/* Home */}
     </div>
   );
 }
