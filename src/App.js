a2827e856b15ff5826fa281ec17846fc43571037
prasanth-kumar1 <prasanth.kumar@iiitb.org>
2021-06-13 15:37:50 +0530

    Completed Payment page UI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { useEffect } from 'react'
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Payment from './Payment'
 import Checkout from './Checkout'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+const promise = loadStripe('pk_test_51J1pIHSGHRgjOalga4ds6FHoEDBV30c7jEWT8xEaGFMAcKOai2JApGydkfvBPMANNOZX78l4u7wGSOnmJ1J6E1sH004UiwSYpN');
 
 function App() {
 
@@ -43,6 +48,12 @@ function App() {
           <Route path='/checkout'>
             <Header />
             <Checkout />
+          </Route>
+          <Route path='/payment'>
+            <Header />
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>
           </Route>
           <Route path='/'>
             <Header />
